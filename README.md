1c8ef42ecf39efd13dc57f399d02c80d4dfc6f61
Deepanshu Singh <deepanshu2002d@gmail.com>
2021-01-12 20:28:24 +0530

    TIC TAC TOE Added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tictactoe
+
+Hello fellows. This is a TicTacToe game program written in python and accessible via CLI.
+
+PS C:\Users\hp\Desktop\tictactoe> python tictactoe.py
+
---
+++ tictactoe.py
@@ -0,0 +1,133 @@
+board = [' ' for x in range(10)]
+
+def insert(letter,pos):
+    board[pos] = letter
+
+def freeSpace(pos):
+    return board[pos] == ' '
+
+def printBoard(board):
+    print('   |   |   ')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |   ')
+    print('------------')
+    print('   |   |   ')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |   ')
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def winner(b,l):    # b = board and l = letter 
+    return ((b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l))
+
+def playerMove():
+    run = True
+    while run:
+        move = input("Please select a position between 1 to 9 to enter X ")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if freeSpace(move):
+                    run = False
+                    insert('X' , move)
+                else:
+                    print('Position already occupied')
+            else:
+                print('please give number between 1-9')
+
+        except:
+            print('Please type a number')
+
+def computerMove():
+    possibleMoves = [x for x , letter in enumerate(board) if letter == ' ' and x != 0  ]
+    move = 0
+
+    for let in ['O' , 'X']:
+        for i in possibleMoves:
+            boardcopy = board[:]
+            boardcopy[i] = let
+            if winner(boardcopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1 , 3 , 7 , 9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+
+def main():
+    print("Welcome!!!")
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(winner(board , 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print("sorry, you loose!")
+            break
+
+        if not(winner(board , 'X')):
+            move = computerMove()
+            if move == 0:
+                print(" ")
+            else:
+                insert('O' , move)
+                print('computer placed an o on position' , move , ':')
+                printBoard(board)
+        else:
+            print("you win!")
+            break
+
+
+
+
+    if isBoardFull(board):
+        print("It's a tie")
+
+while True:
+    x = input("Do you want to play the game? (y/n)")
+    if x.lower() == 'y':
+        board = [' ' for x in range(10)]
+        print(' ', end='')
+        main()
+    else:
+        break
